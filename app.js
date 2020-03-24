c4454d1577702506b9b2df8bcef85635c420debf
Bryan Hickey <bh@bigmanmedia.com.au>
2020-03-24 14:30:56 +1100

    lesson 15

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 new Vue({
     el: '#app',
     data: {
-        title: 'Becoming a Vue ninja',
-        name: 'Bryan',
+        title: "Becomeing a Vue ninja",
+        items: [
+            '🍄 Mushroom', '🐚 Green Shells', '🦞 Red Lobster', '🍌 Banana', '⭐ Star'
+        ],
+        ninjas: [
+            { name: 'Crystal', age: 35, belt: 'Black' },
+            { name: 'Angel', age: 25, belt: 'Purple' },
+            { name: 'Isabella', age: 45, belt: 'Blue' },
+            { name: 'Angel', age: 15, belt: 'Brown' }
+        ]
     },
     methods: {
-        logMessage(){
-            console.log('Hello, world!');
-        }
     }
 })
