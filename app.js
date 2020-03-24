fd368da95b4a78b20058215eca0d22582f76acfe
Bryan Hickey <bh@bigmanmedia.com.au>
2020-03-24 14:15:33 +1100

    lesson 11-14

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
 new Vue({
     el: '#app',
     data: {
-        title: 'Bryan',
-        url: 'https://youtube.com',
-        classes: ['one', 'two'],
-        coords: {
-            x:0,
-            y:0
-        }
+        title: 'Becoming a Vue ninja',
+        name: 'Bryan',
     },
     methods: {
-        logEvent(e) {
-            console.log(e)
-        },
-        logCoords(e){
-            this.coords.x = e.offsetX
-            this.coords.y = e.offsetY
+        logMessage(){
+            console.log('Hello, world!');
         }
     }
 })
